51748e5c12f2f6f3494d4a74c45d67ac195be2e2
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-05 19:16:59 +0545

    add chi router

--- main.go
+++ main.go
@@ -2,67 +2,46 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
+	"log"
 	"net/http"
+	"reflect"
 )
 
-type Person struct {
-	name string
-}
-
 func main() {
-	fmt.Println("Server started at port 9000")
-	if err := http.ListenAndServe(":9000", http.HandlerFunc(handler)); err != nil {
-		fmt.Println("Error encountered during server start", err)
+	router := chi.NewRouter()
+	router.Use(chiMiddleware.Logger)
+	router.Use(chiMiddleware.RealIP)
+	handler(router)
+	fmt.Println("Server started at port 3000")
+	if err := http.ListenAndServe(":3000", router); err != nil {
+		panic("Failed to start server.")
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println(r.Method, r.URL, r.Host, r.UserAgent(), r.Header)
+func handler(router chi.Router) {
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("<h1>Hello World</h1>"))
+		if err != nil {
+			log.Fatal("Error writing.")
+		}
+	})
+	// Named Param /{id}. Jus like /:id in Express
+	// Query. /?abc=def&abc=def -> abc=[def ghi]
 
-	// URL.Path always returns decoded value
-	// URL.RawPath returns the exact same encoded value. For eg. Type /contact%2f. Here %2f is encoded value of "/"
-	fmt.Println(r.URL.Path, r.URL.RawPath, r.URL.Query())
-	switch r.URL.Path {
-	case "/":
-		{
-			person := Person{name: "Niraj"}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(200)
-			if _, err := fmt.Fprint(w, person); err != nil {
-				return
-			}
-			break
+	router.Get("/user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		fmt.Println(query["abc"], reflect.TypeOf(query["abc"]))
+		userId := chi.URLParam(r, "id")
+		_, err := w.Write([]byte(fmt.Sprintf("<h1>Hello User %s</h1>", userId)))
+		if err != nil {
+			log.Fatal("Error writing.")
 		}
-	case "/contact":
-		{
-			contactHandler(w, r)
-			break
-		}
-	case "/faq":
-		{
-			faqHandler(w, r)
-			break
-		}
-	default:
-		{
-			w.WriteHeader(404)
-			if _, err := fmt.Fprint(w, "<h1>Page not found</h1>"); err != nil {
-				return
-			}
-		}
-	}
-}
+	})
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println(r.Cookies())
-	//fmt.Println(r.Cookie("csrf"))
-	if _, err := fmt.Fprint(w, "<h1>Contact Us </h1>"); err != nil {
-		return
-	}
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	if _, err := fmt.Fprint(w, `<h1>FAQ Page</h1>`); err != nil {
-		return
-	}
+	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte("<h1>Page not found</h1>")); err != nil {
+		}
+	})
 }
